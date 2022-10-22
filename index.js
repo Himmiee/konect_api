65a5949a2602c34d0e5180094bbd228f225ab70f
Haliyah Tijani <haliyya460@gmail.com>
2022-10-22 22:43:10 +0100

    upd commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/ngo',ngoRoutes)
 app.use(cors({
-    origin : "http://localhost:3000"
+    origin : "*"
 }))
 
 
